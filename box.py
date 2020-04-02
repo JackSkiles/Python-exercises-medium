14e6a1f361a1032e4ee8a489e88e72313f4f2004
Jack Skiles <jackskiles420@gmail.com>
2020-04-02 15:46:47 -0400

    Add new program box.py

--- box.py
+++ box.py
@@ -1,5 +1,3 @@
-
-# Asks user for input to decide the square's height and width
 square_height = int(input('Please input how tall you would like your square to be'))
 
 square_width = int(input('Please input how wide you would like your square to be.'))
@@ -8,14 +6,32 @@ square_width = int(input('Please input how wide you would like your square to be
 counter = 0
 # Creates an empty string that will be filled with line breaks and *'s
 square_creator = ''
+# A second counter to be used later on
+counter2 = 0
+# Creates an if statement to check if it is the first line. If it is it procedes with the loop to add a line  of *'s based on user input.
+if counter == 0:
+    while counter <= square_width:
+        square_creator += ' * '
+        counter += 1
+    square_creator += '\n'
 
-# Forms the loop that goes for however long the input sepecified. Looping through the width portion each time 
-# there is a line break.
-while counter < square_height:
-    counter2 = 0
-    while counter2 <= square_width:
-        square_creator += ' '
-        counter2 += 1
-    print('*')
-    counter += 1
+# Using a second and third counter these nested loops add * and empty spaces every row based on user input. After the nested loop runs a * and line break occurs and then another * is printed
+# on the start of the outer loop.
+while counter2 <= square_height:
+    counter3 = 0
+    square_creator += ' * '
+   
+    while counter3 <= square_width:
+        square_creator += '  '
+        counter3 += 1
+    square_creator += ' *\n'
+    counter2 += 1
+# Using a fourth counter mimics the start of the box and prints a straight line of * based on user input.
+counter4 = 0
+
+if counter4 == 0:
+    while counter4 <= square_width:
+        square_creator += ' * '
+        counter4 += 1
 print(square_creator)
+
