7d62defc6ec63acb4c9703569b6bebddb7d96cbc
Jack Skiles <jackskiles420@gmail.com>
2020-04-02 16:30:23 -0400

    Bug fixes

--- box.py
+++ box.py
@@ -3,33 +3,36 @@ square_height = int(input('Please input how tall you would like your square to b
 square_width = int(input('Please input how wide you would like your square to be.'))
 
 # Creates a counter to increment line breaks for the height portion
-counter = 0
+counter = 1
 # Creates an empty string that will be filled with line breaks and *'s
-square_creator = ''
+square_creator = ' '
 # A second counter to be used later on
-counter2 = 0
+counter2 = 2
 # Creates an if statement to check if it is the first line. If it is it procedes with the loop to add a line  of *'s based on user input.
-if counter == 0:
+if counter == 1:
     while counter <= square_width:
         square_creator += ' * '
         counter += 1
-    square_creator += '\n'
+    square_creator += ' \n '
 
-# Using a second and third counter these nested loops add * and empty spaces every row based on user input. After the nested loop runs a * and line break occurs and then another * is printed
-# on the start of the outer loop.
-while counter2 <= square_height:
-    counter3 = 0
-    square_creator += ' * '
-   
+# Using a second and third counter these nested loops add * and empty spaces every row based on user input. An if statement occurs in the nested loop to check if the count
+# is the first or last and prints * and spaces accordingly.
+while counter2 < square_height:
+    counter3 = 1
     while counter3 <= square_width:
-        square_creator += '  '
+        if counter3 == 1:
+            square_creator += ' * '
+        elif counter3 == square_height:
+            square_creator += ' * '
+        else:
+            square_creator += '   '
         counter3 += 1
-    square_creator += ' *\n'
+    square_creator += '\n ' 
     counter2 += 1
 # Using a fourth counter mimics the start of the box and prints a straight line of * based on user input.
-counter4 = 0
+counter4 = 1
 
-if counter4 == 0:
+if counter4 == 1:
     while counter4 <= square_width:
         square_creator += ' * '
         counter4 += 1
